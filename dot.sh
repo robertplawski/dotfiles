20b5ee305f2aa823e20f90dbfeedd22735802737
Robert Plawski <dev@robertplawski.pl>
2025-01-22 23:30:20 +0100

    Added automated script to download and apply config

---
+++ dot.sh
@@ -0,0 +1,46 @@
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/debian_version]=apt-get
+
+echo "https://robertplawski.pl/dot.sh"
+echo "By Robert Plawski 2025"
+echo ""
+
+dependencies="qtile picom rofi firefox git alacritty nvim"
+
+if [ $(id -u) -ne 0 ]
+	then echo "Please run as root or using sudo"
+	exit
+fi
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        echo Package manager detected - ${osInfo[$f]}
+	echo Installing dotfiles in 3 seconds...
+	echo ""
+	sleep 3
+	echo Installing requested configuration
+	#for dependency in ${dependencies[@]}
+	#do
+	if [[ "${osInfo[$f]}" == "yum" ]]; then
+		yum install $dependencies -y
+	fi
+	if [[ "${osInfo[$]}" == "pacman" ]]; then
+		pacman --noconfirm -S $dependencies
+	fi
+	if [[ "${osInfo[$]}" == "apt-get" ]]; then
+		apt-get install $dependencies -y
+	fi
+	#done
+	echo Downloading configuration
+	git clone https://github.com/robertplawski/dotfiles.git
+	cd dotfiles
+	cp ./* -r ~/.config
+	echo Applying configuration
+	echo Done! Enjoy your new configured system
+	
+    fi
+done
+

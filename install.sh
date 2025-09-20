1e52b80be4336c74042b27c93d93fd60380ff9e2
Robert Pławski <dev@robertplawski.pl>
2025-09-20 15:20:17 +0200

    added thinclient to install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ sudo dnf -y install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+info "Starting thinclient setup"
+./thinclient/setup.sh 
+
 ask "Do you want to install NVIDIA drivers?"
 read -r answer
 if lspci | grep -i nvidia; then

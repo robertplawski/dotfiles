fb719732942dfccc1648971c77a72d365b2969b5
Robert Pławski <dev@robertplawski.pl>
2025-11-24 23:29:05 +0100

    added flatpak easyeffects

--- install.sh
+++ install.sh
@@ -103,6 +103,7 @@ if [[ "$answer" =~ ^[Yy]$ ]]; then
   info "Installing flatpak and apps..."
   sudo dnf install flatpak
   sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  flatpak install flathub com.github.wwmm.easyeffects -y
   flatpak install flathub com.spotify.Client -y
   flatpak install flathub com.parsecgaming.parsec -y
   flatpak install flathub com.bitwarden.desktop -y
